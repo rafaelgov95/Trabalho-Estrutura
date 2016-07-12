6eb1baf317f75e8e8e4f48540ad7e19554d46f79
Higor Oliveira <Higor Oliveira>
2016-07-12 09:48:15 -0400

    Update MetodoTrocar

---
+++ Trabalho_I/src/Troca/MetodoTrocar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Troca;
+
+public class MetodoTrocar {
+    
+    //Método usado para as trocas em todos os Sorts
+    public int[] MetodoTrocar(int vet[], int a, int b) {
+        
+        int aux = vet[a];
+        vet[a] = vet[b];
+        vet[b] = aux;
+        
+        return vet;
+    }
+}
